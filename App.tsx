6d720616b1d8e1c8fa0f28c63480c4ad4a50c7b9
joshdlcruz <dcjoshua25@gmail.com>
2020-11-03 10:12:58 +0800

    initial commit

---
+++ App.tsx
@@ -0,0 +1,233 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the UI Kitten TypeScript template
+ * https://github.com/akveo/react-native-ui-kitten
+ *
+ * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ImageProps,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+import {
+  ApplicationProvider,
+  Button,
+  Icon,
+  IconRegistry,
+  Layout,
+  Text,
+} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
+
+/**
+ * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
+ * https://akveo.github.io/eva-icons
+ */
+const HeartIcon = (
+  props?: Partial<ImageProps>,
+): React.ReactElement<ImageProps> => <Icon {...props} name="heart" />;
+
+const screenWidth = Dimensions.get('window').width;
+
+const chartConfig = {
+  backgroundGradientFrom: '#1E2923',
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: '#08130D',
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false, // optional
+};
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43],
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+      strokeWidth: 2, // optional
+    },
+  ],
+  legend: ['Rainy Days'], // optional
+};
+
+const dataProgress = {
+  labels: ['Swim', 'Bike', 'Run'], // optional
+  data: [0.4, 0.6, 0.8],
+};
+
+const dataBar = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43],
+    },
+  ],
+};
+
+const dataStackedBar = {
+  labels: ['Test1', 'Test2'],
+  legend: ['L1', 'L2', 'L3'],
+  data: [
+    [60, 60, 60],
+    [30, 30, 60],
+  ],
+  barColors: ['#dfe4ea', '#ced6e0', '#a4b0be'],
+};
+
+const dataPie = [
+  {
+    name: 'Seoul',
+    population: 21500000,
+    color: 'rgba(131, 167, 234, 1)',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'Toronto',
+    population: 2800000,
+    color: '#F00',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'Beijing',
+    population: 527612,
+    color: 'red',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'New York',
+    population: 8538000,
+    color: '#ffffff',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'Moscow',
+    population: 11920000,
+    color: 'rgb(0, 0, 255)',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+];
+
+const commitsData = [
+  {date: '2017-01-02', count: 1},
+  {date: '2017-01-03', count: 2},
+  {date: '2017-01-04', count: 3},
+  {date: '2017-01-05', count: 4},
+  {date: '2017-01-06', count: 5},
+  {date: '2017-01-30', count: 2},
+  {date: '2017-01-31', count: 3},
+  {date: '2017-03-01', count: 2},
+  {date: '2017-04-02', count: 4},
+  {date: '2017-03-05', count: 2},
+  {date: '2017-02-30', count: 4},
+];
+
+export default (): React.ReactFragment => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <Layout style={styles.container}>
+        <Text>Line Chart</Text>
+        <LineChart
+          data={data}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+        />
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={data}
+          width={screenWidth}
+          height={256}
+          verticalLabelRotation={30}
+          chartConfig={chartConfig}
+          bezier
+        />
+        <Text>Progress Ring</Text>
+        <ProgressChart
+          data={dataProgress}
+          width={screenWidth}
+          height={220}
+          strokeWidth={16}
+          radius={32}
+          chartConfig={chartConfig}
+          hideLegend={false}
+        />
+        <Text>Bar Chart</Text>
+        <BarChart
+          data={dataBar}
+          width={screenWidth}
+          height={220}
+          yAxisLabel="$"
+          chartConfig={chartConfig}
+          verticalLabelRotation={30}
+        />
+        <Text>Stacked Bar Chart</Text>
+        <StackedBarChart
+          data={dataStackedBar}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+        />
+        <Text>Pie Chart</Text>
+        <PieChart
+          data={dataPie}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+          accessor="population"
+          backgroundColor="transparent"
+          paddingLeft="15"
+          absolute
+        />
+        <Text>Contribution graph</Text>
+        <ContributionGraph
+          values={commitsData}
+          endDate={new Date('2017-04-01')}
+          numDays={105}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+        />
+      </Layout>
+    </ApplicationProvider>
+  </>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+  likeButton: {
+    marginVertical: 16,
+  },
+});
